e658c060d0977d269b721c3ce6e52e708aed58c4
yangdao <458424869@qq.com>
2023-06-28 13:17:53 +0800

    完成Track

--- src/main/scala/ui/controls/Track.scala
+++ src/main/scala/ui/controls/Track.scala
@@ -9,79 +9,129 @@ import ui.core.Rect2D
 import ui.core.FreeLayout
 import ui.core.FreeLayoutItem
 import core.UpdateMgr
+import ui.core.Orientation
 
 class Track extends Control derives ReflectType {
-    private var startBtn:RepeatButton = RepeatButton();
-    private var endBtn:RepeatButton = RepeatButton();
-    private var thumb:Thumb = Thumb();
+  var _orientation: Orientation = Orientation.Horizontal;
+  var _value: Float = 0;
 
-    private var cacheSize:Vector2 = Vector2.zero.clone();
-    private var curPosRate:Float = 0;
-    override def Awake(): Unit = {
-        super.Awake();
-        this.thumb.OnBeginDragCall = Some(this.OnStartDrag);
-        this.thumb.OnDragCall = Some(this.OnDrag);
-        this.thumb.OnEndDragCall = Some(this.OnEndDrag);
-    }
-    override def Enter(): Unit = {
-        super.Enter();
-        this.children.foreach {child => 
-            child.getEntity().get.add[FreeLayoutItem]();    
-        };
-    }
+  private var startBtn: RepeatButton = RepeatButton();
+  private var endBtn: RepeatButton = RepeatButton();
+  private var thumb: Thumb = Thumb();
 
-    override def OnEnter(): Unit = {
-        this.createEntity();
-        this.loadControlTemplate();
-        this.addChild(this.thumb);
-        UpdateMgr.add(this.OnUpdate);
-        this.cacheSize.x = this.width.getPixel().getOrElse(0);
-        this.cacheSize.y = this.height.getPixel().getOrElse(0);
-    }
+  private var cacheSize: Vector2 = Vector2.zero.clone();
+  private var curPosRate: Float = 0;
 
-    protected def createEntity():Unit = {
-        val parentEntity = this.parent.flatMap(_.getEntity());
-        val newEntity = Entity.spawnEmpty()
-                              .add[Transform](_.parent = parentEntity)
-                              .add[Rect2D]()
-                              .add[FreeLayout](v => {
-                                v.common.hor = this._hor;
-                                v.common.ver = this._ver;
-                                v.common.uiSize.width = this._width;
-                                v.common.uiSize.height = this._height;
-                                v.common.padding = this._padding;
-                                v.common.margin = this._margin;
-                              });
-        this.entity = Some(newEntity);
-    }
+  def orientation: Orientation = _orientation;
+  def orientation_=(value: Orientation): Unit = {
+    _orientation = value; this.callPropertyChanged("orientation", this);
+  }
 
-    protected def OnStartDrag(pos:Vector2):Unit = {
-    }
+  def value: Float = this._value;
+  def value_=(value: Float): Unit = {
+    _value = value; this.callPropertyChanged("value", this)
+    this.updatePosByValue();
+  }
 
-    protected def OnDrag(delta:Vector2):Unit = {
-        println(delta);
-    }
+  override def Awake(): Unit = {
+    super.Awake();
+    this.thumb.OnBeginDragCall = Some(this.OnStartDrag);
+    this.thumb.OnDragCall = Some(this.OnDrag);
+    this.thumb.OnEndDragCall = Some(this.OnEndDrag);
+  }
+  override def Enter(): Unit = {
+    super.Enter();
+    this.thumb.getEntity().get.add[FreeLayoutItem]();
+  }
 
-    protected def OnEndDrag(pos:Vector2):Unit = {
+  override def OnEnter(): Unit = {
+    this.createEntity();
+    this.loadControlTemplate();
+    this.addChild(this.thumb);
+    UpdateMgr.add(this.OnUpdate);
+    this.cacheSize.x = this.width.getPixel().getOrElse(0);
+    this.cacheSize.y = this.height.getPixel().getOrElse(0);
+  }
 
-    }
+  protected def createEntity(): Unit = {
+    val parentEntity = this.parent.flatMap(_.getEntity());
+    val newEntity = Entity
+      .spawnEmpty()
+      .add[Transform](_.parent = parentEntity)
+      .add[Rect2D]()
+      .add[FreeLayout](v => {
+        v.common.hor = this._hor;
+        v.common.ver = this._ver;
+        v.common.uiSize.width = this._width;
+        v.common.uiSize.height = this._height;
+        v.common.padding = this._padding;
+        v.common.margin = this._margin;
+      });
+    this.entity = Some(newEntity);
+  }
 
-    protected def OnUpdate(dt:Float):Unit = {
-       val rawRect = this.getEntity().get.get[Rect2D]();
-       val x = rawRect._1;
-       val y = rawRect._2;
-       if(this.cacheSize.x != x && this.cacheSize.y != y) {
-          this.cacheSize.x = x;
-          this.cacheSize.y = y;
-          this.onLayoutResize();
-       }
-    }
+  protected def OnStartDrag(pos: Vector2): Unit = {}
 
-    protected def onLayoutResize():Unit = {
+  protected def OnDrag(delta: Vector2): Unit = {
+    val freeItem = this.thumb.getEntity().get.get[FreeLayoutItem]();
+    val thisRect = this.getEntity().get.get[Rect2D]();
+    val thumbRect = this.thumb.getEntity().get.get[Rect2D]();
+    this._orientation match {
+      case Orientation.Horizontal => {
+        val newX = freeItem._1 + delta.x;
+        val maxPos = thisRect._1 - thumbRect._1;
+        if (newX >= 0 && newX <= maxPos) {
+          freeItem._1 = newX;
+        }
+        this._value = newX / maxPos;
+      }
+      case Orientation.Vertical => {
+        val newY = freeItem._2 + (-delta.y);
+        val maxPos = thisRect._2 - thumbRect._2;
+        if (newY >= 0 && newY <= maxPos) {
+          freeItem._2 = newY;
+        }
+        this._value = newY / maxPos;
+      }
     }
+    this.callPropertyChanged("value",this);
+  }
 
-    override def Exit(): Unit = {
-        super.Exit();
-        UpdateMgr.remove(this.OnUpdate);
+  protected def OnEndDrag(pos: Vector2): Unit = {}
+
+  protected def OnUpdate(dt: Float): Unit = {
+    val rawRect = this.getEntity().get.get[Rect2D]();
+    val x = rawRect._1;
+    val y = rawRect._2;
+    if (this.cacheSize.x != x || this.cacheSize.y != y) {
+      this.cacheSize.x = x;
+      this.cacheSize.y = y;
+      this.onLayoutResize();
     }
+  }
+
+  protected def updatePosByValue(): Unit = {
+    val freeItem = this.thumb.getEntity().get.get[FreeLayoutItem]();
+    val thisRect = this.getEntity().get.get[Rect2D]();
+    val thumbRect = this.thumb.getEntity().get.get[Rect2D]();
+    this._orientation match {
+      case Orientation.Horizontal => {
+        val maxPos = thisRect._1 - thumbRect._1;
+        freeItem._1 = maxPos * this._value;
+      }
+      case Orientation.Vertical => {
+        val maxPos = thisRect._2 - thumbRect._2;
+        freeItem._2 = maxPos * this._value;
+      }
+    }
+  }
+
+  protected def onLayoutResize(): Unit = {
+    this.updatePosByValue();
+  }
+
+  override def Exit(): Unit = {
+    super.Exit();
+    UpdateMgr.remove(this.OnUpdate);
+  }
 }
