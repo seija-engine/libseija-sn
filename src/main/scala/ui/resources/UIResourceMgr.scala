7d620bb00174045a7ac11006ac4171d8a9e3b658
seija <458424869@qq.com>
2023-09-03 19:08:25 +0800

    sxml object parse

--- src/main/scala/ui/resources/UIResourceMgr.scala
+++ src/main/scala/ui/resources/UIResourceMgr.scala
@@ -1,14 +1,8 @@
 package ui.resources
-import core.logError
-import ui.xml.XmlObjectParser
-import ui.xml.XmlNSResolver
-import core.xml.XmlElement
 import ui.xml.UISXmlEnv
 
 object UIResourceMgr {
   var appResource: UIResource = UIResource.empty()
-
- 
   def loadScriptResource(path: String): Unit = {
     val evalValue = UISXmlEnv.evalFile(path).get
     val resList = evalValue.toScalaValue().asInstanceOf[Vector[Any]]
--- src/main/scala/ui/xml/SXmlObjectParser.scala
+++ src/main/scala/ui/xml/SXmlObjectParser.scala
@@ -1,9 +1,12 @@
 package ui.xml
 import sxml.vm.XmlNode
+import core.logError
 import scala.util.Try
-import core.reflect.TypeInfo
+import core.reflect.{DynTypeConv, TypeInfo}
 import sxml.vm.VMValue
+import ui.binding.BindingItem
 import ui.controls.UIElement
+import ui.resources.UIResourceMgr
 
 class SXmlObjectParser(val nsResolver: XmlNSResolver = XmlNSResolver.default) {
   def parse(xml: XmlNode): Try[Any] = Try {
@@ -13,15 +16,51 @@ class SXmlObjectParser(val nsResolver: XmlNSResolver = XmlNSResolver.default) {
   }
 
   def _parse(xml: XmlNode): Try[Any] = Try {
+
     val curTypeInfo = nsResolver.resolverTypeInfo(xml.Name).get
     val curObject = curTypeInfo.create()
-    for((k,v) <- xml.attrs) {
-        setStringProp(curTypeInfo,curObject,k,v)
+
+    for ((k, v) <- xml.attrs) {
+      setStringProp(curTypeInfo, curObject, k, v).logError()
     }
+
+    var contentCount = 0;
+    for(childElem <- xml.child) {
+      
+    }
+
     curObject
   }
 
-  def setStringProp(typInfo:TypeInfo,curObject:Any,key:String,value:VMValue):Unit = {
+  def setStringProp(typInfo: TypeInfo,curObject: Any,key: String,value: VMValue): Try[Unit] = Try {
     val isUIElement = curObject.isInstanceOf[UIElement]
+    val stringValue: String = value.unwrap[VMValue.VMString]().get.value
+    if (isUIElement && key.startsWith("{Binding")) {
+      val curUIElement = curObject.asInstanceOf[UIElement]
+      curUIElement.addBindItem(BindingItem.parse(key, stringValue).get)
+    } else if (key.startsWith("{Res")) {
+      val startLen = "{Res".length()
+      var resName = stringValue.substring(startLen, stringValue.length - 1)
+      resName = resName.trim()
+      this.setRes(resName, typInfo, key, curObject)
+    } else {
+      (for {
+        filed <- typInfo.getFieldTry(key)
+        value <- DynTypeConv.strConvertToTry(filed.typName, stringValue)
+      } yield filed.set(curObject, value)).get
+    }
   }
+
+  def setRes(resName: String,typeInfo: TypeInfo,key: String,curObject: Any): Unit = {
+    UIResourceMgr.appResource.findRes(resName).foreach { res =>
+      typeInfo.getField(key).foreach { f =>
+        DynTypeConv
+          .convertStrTypeTry(res.getClass.getName, f.typName, res)
+          .foreach { v =>
+            f.set(curObject, v)
+          }
+      }
+    }
+  }
+
 }
