2f133ee5dfd4cf51a95a6a18c835ea6bfede70d3
yangdao <458424869@qq.com>
2023-12-28 10:16:14 +0800

    treeview

---
+++ src/main/scala/com/seija/ui/controls/TreeView.scala
@@ -0,0 +1,7 @@
+package com.seija.ui.controls
+import com.seija.core.reflect.ReflectType;
+import com.seija.ui.controls.ItemsControl
+
+class TreeView extends ItemsControl derives ReflectType {
+    
+}
---
+++ src/main/scala/com/seija/ui/controls/TreeViewItem.scala
@@ -0,0 +1,6 @@
+package com.seija.ui.controls
+import com.seija.core.reflect.ReflectType;
+
+class TreeViewItem extends HeaderedItemsControl derives ReflectType {
+    
+}
